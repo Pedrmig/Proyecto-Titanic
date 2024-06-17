4793a0417f59bb449d374876e7337d302cb47e2e
Pedrmig <pedro.ferraz10@hotmail.com>
2024-06-17 09:38:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if selected == "Datos":
     st.write(df.head())
 
     st.header("Analisis de valores nulos")
-    st.write(((df.isnull().sum() / len(df))*100).to_frame().T)
+    st.write((((df.isnull().sum() / len(df))*100).apply(lambda x:f'{x:2f}%')).to_frame().T)
 
     st.header("Limpieza de los Datos")
 
